2b35cc319ec58c52bd8d44ed736e3aa7c6e0c6e7
Npranata <nathannelson348@gmail.com>
2023-06-28 21:45:00 -0400

    Created next and previous buttons to go to next pages

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const APIURL=  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bdc5c5d47d4a215c6ac5150d5018e676"
+const APIURL=  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bdc5c5d47d4a215c6ac5150d5018e676&page="
 const APIKEY ="api_key=bdc5c5d47d4a215c6ac5150d5018e676"
 const IMGPATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=bdc5c5d47d4a215c6ac5150d5018e676&query="
 const form = document.getElementById("form")
 const search = document.getElementById("searchBar")
+const PAGE_COUNT = 5;
+let currentPage= 1;
+let totalPageCount = 0;
+let moviesPerPage = [];
+
+
+
+
+
+
+function removeMovieFromPage(movieId, page) {
+  const movies = moviesPerPage[page];
+  const filteredMovies = movies.filter(movie => movie.id !== movieId);
+  moviesPerPage[page] = filteredMovies; // Update movies for the current page
+  showMovies(filteredMovies); // Update the displayed movies on the page
+
+}
 
 /*
 * Fetches data from TMDB's API and return the parsed response data
 */ 
-async function getMovies(url) {
-    const resp = await fetch(url);
-    const respData = await resp.json();
-    console.log(respData)
-    showMovies(respData.results)
+async function getMovies(url, page) {
+  const resp = await fetch(url + page);
+  const respData = await resp.json();
+  console.log(respData);
+  totalPageCount = respData.total_pages; // Update the total number of pages
+  moviesPerPage[page]= respData.results
+  showMovies(respData.results);
+  removeMovieFromPage(1127227, 3)
+    
   }
 
 
 
 function showMovies(movies){
   const main = document.getElementById("main")
-  // //make main is cleared
+  // main is cleared
   main.innerHTML =' '
 
   //object destructuring 
@@ -35,18 +56,21 @@ function showMovies(movies){
         <span class="${getRateColor(ROUNDVOTE)}">${ROUNDVOTE}</span>
       </div> 
       <div class="overview"> 
+        <h4> Overview </h4>
         ${overview}
       </div>
     `
     main.appendChild(movieEl)
   })
 }
-
   /* 
   * Returns rating colors beased on rating scale
   */
   function getRateColor(vote){
-    if(vote>=8){
+    if(vote==0){
+      return
+    }
+    else if(vote>=8){
       return 'green'
     }
     else if(vote >= 5 ){
@@ -77,6 +101,26 @@ function showMovies(movies){
   }
 
 
-getMovies(APIURL)
+  function handlePrevPage() {
+    if (currentPage > 1) {
+      currentPage--;
+      getMovies(APIURL, currentPage);
+    }
+  }
+  
+  function handleNextPage() {
+    if (currentPage < totalPageCount) {
+      currentPage++;
+      getMovies(APIURL, currentPage);
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("prevPage").addEventListener("click", handlePrevPage);
+    document.getElementById("nextPage").addEventListener("click", handleNextPage);
+  })
+
+getMovies(APIURL, currentPage)
 movieSearch()
+
 
