0a2cd7145a261a76809013c4d199b76fef8fd952
Npranata <nathannelson348@gmail.com>
2023-07-01 16:49:35 -0400

    Created a bar that shows movie genres

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const APIURL=  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bdc5c5d47d4a215c6ac5150d5018e676&page="
+const APIURL2=  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bdc5c5d47d4a215c6ac5150d5018e676"
 const APIKEY ="api_key=bdc5c5d47d4a215c6ac5150d5018e676"
 const IMGPATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=bdc5c5d47d4a215c6ac5150d5018e676&query="
 const form = document.getElementById("form")
 const search = document.getElementById("searchBar")
-const genre = "https://api.themoviedb.org/3/genre/movie/list?api_key=bdc5c5d47d4a215c6ac5150d5018e676&language=en-US"
+const GENRE = "https://api.themoviedb.org/3/genre/movie/list?api_key=bdc5c5d47d4a215c6ac5150d5018e676&language=en-US"
 let currentPage= 1;
 let totalPageCount = 0;
 let moviesPerPage = [];
@@ -28,12 +29,14 @@ async function getMovies(url, page) {
   console.log(respData);
   totalPageCount = respData.total_pages; // Update the total number of pages
   moviesPerPage[page]= respData.results
-  // showMovies(respData.results);
   const movies = moviesPerPage[page];
   const filteredMovies = movies.filter(movie => movie.poster_path !== null);
   moviesPerPage[page] = filteredMovies; // Update movies for the current page
   showMovies(filteredMovies); // Update the displayed movies on the page
   }
+
+  
+  
 
 
 
@@ -124,7 +127,7 @@ function showMovies(movies){
   })
 
   document.addEventListener('DOMContentLoaded', () => {
-    const navbarIcon = document.querySelector('.navbar-icon');
+    const navbarIcon = document.querySelector('.navbar');
     const whiteBox = document.getElementById('whiteBox');
 
     navbarIcon.addEventListener('click', () => {
@@ -141,9 +144,46 @@ function showMovies(movies){
     });
     
 });
+function handleGenreSelection(genreId) {
+  selectedGenre = genreId;
+  currentPage = 1;
+  getMovies(GENRE, currentPage);
+}
+
+// Fetch genre list and populate genre dropdown
+async function fetchGenres(genre) {
+  const resp = await fetch(genre);
+  const respData = await resp.json();
+  const genres = respData.genres;
+  console.log(genres)
+  showMovieGenre(genres)
+  const allGenres =  document.getElementById("genreNames");
+  document.addEventListener('click', (event) => {
+  genreNames.addEventListener('click', () => {
+    handleGenreSelection(GENRE);
+  });
+})
+}
+
+function showMovieGenre(genres){
+  const boxElement = document.getElementById("whiteBox");
+
+  genres.forEach((genre)=> {
+    const{id, name}= genre;
+    genreEl = document.createElement('genreTypes');
+    genreEl.classList.add('genreTypes');
+    genreEl.innerHTML= `
+    <div> ${name}</div>
+    
+    `
+    boxElement.appendChild(genreEl)
+  })
+}
+
+
+ 
 
 
 getMovies(APIURL, currentPage)
 movieSearch()
-
-
+fetchGenres(GENRE) 
